81f5764f7e1c5e0c59639f492442cd3e5da85248
santi <santimartinezalbert02@gmail.com>
2023-01-19 16:23:23 +0100

    testing change readme custom action

---
+++ .github/actions/changereadme/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fs = require("fs");
+
+const readme = path.resolve('./README.md')
+const resultado = process.env.resultado;
+let URL = resultado == 'success' ? 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg' : 'https://img.shields.io/badge/test-failure-red';
+URL = "![Cypress.io](" + URL + ")"
+
+fs.readFile(readme, 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log(data);
+    data += URL;
+    fs.writeFile(readme, data, function (err) {
+        if (err) throw err;
+        console.log('Archivo actualizado');
+    })
+});
