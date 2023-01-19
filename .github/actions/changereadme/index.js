82e8a3a52d149f9dd855ac49bbba5d6230d0e247
santi <santimartinezalbert02@gmail.com>
2023-01-19 18:40:20 +0100

    Vercel

--- .github/actions/changereadme/index.js
+++ .github/actions/changereadme/index.js
@@ -3,13 +3,14 @@ const fs = require("fs");
 
 const readme = path.resolve('./README.md')
 const resultado = process.env.resultado;
-let URL = resultado == 'success' ? 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg' : 'https://img.shields.io/badge/test-failure-red';
-URL = "![Cypress.io](" + URL + ")"
+let URL_good = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg'
+let URL_bad = 'https://img.shields.io/badge/test-failure-red'
+let URL = resultado == 'success' ? URL_good : URL_bad;
 
-fs.readFile(readme, 'utf8', function(err, data) {
+fs.readFile(readme, 'utf8', function (err, data) {
     if (err) throw err;
-    data += URL;
-    fs.writeFile(readme, data, function (err) {
+    let new_readme = data.search(URL_good) !== -1 ? data.replace(URL_good, URL) : data.replace(URL_bad, URL)
+    fs.writeFile(readme, new_readme, function (err) {
         if (err) throw err;
         console.log('Archivo actualizado');
     })
